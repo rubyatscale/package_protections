f10c0cb60c2fe380be987ed48fad4a3911185535
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-08-13 19:04:30 -0400

    add note to self

--- lib/rubocop/cop/package_protections/namespaced_under_package_name.rb
+++ lib/rubocop/cop/package_protections/namespaced_under_package_name.rb
@@ -23,6 +23,8 @@ module RuboCop
           match = relative_filename.match(%r{((#{::PackageProtections::EXPECTED_PACK_DIRECTORIES.join("|")})/.*?)/})
           package_name = match && match[1]
 
+          # Get `package_name` using `ParsePackage.package_from_path`.
+          # Subtract the package name and then the rest should be history.
           return if package_name.nil?
 
           return if relative_filepath.extname != '.rb'
